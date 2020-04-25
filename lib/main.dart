9545995490d0d7418a24ac29d23f485886b570b9
Madhav Paudel <poudelmadhav143@gmail.com>
2020-04-25 20:54:18 +0545

    top part with name and bio designed

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,27 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           title: Text("My CV App"),
         ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      // backgroundColor: Colors.green,
+                      radius: 60.0,
+                    ),
+                    Text("Madhav Paudel"),
+                    Text("Software Engineer"),
+                    Text("Hello, I am Software Engineer at Namesace Inc. I spefically work of web development. But are is not limited. Sometimes I work on AWS. And sometimes I work on frontend using React too. I have worked on many projects using gulp."),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
