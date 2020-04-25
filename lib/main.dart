263aca81734127e7eb6b01a7ac9b49a9161d5afa
Madhav Paudel <poudelmadhav143@gmail.com>
2020-04-25 21:08:30 +0545

    top part font decorated and spacing fixed

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,46 @@ class MyApp extends StatelessWidget {
           children: <Widget>[
             Card(
               child: Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30.0,
+                  vertical: 30.0,
+                ),
                 child: Column(
                   children: <Widget>[
                     CircleAvatar(
-                      backgroundImage: AssetImage("assets/Madhav_Paudel_photo_square.jpg"),
+                      backgroundImage:
+                          AssetImage("assets/Madhav_Paudel_photo_square.jpg"),
                       radius: 60.0,
                     ),
-                    Text("Madhav Paudel"),
-                    Text("Software Engineer"),
-                    Text("Hello, I am Software Engineer at Namesace Inc. I spefically work of web development. But are is not limited. Sometimes I work on AWS. And sometimes I work on frontend using React too. I have worked on many projects using gulp."),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Text(
+                      "Madhav Paudel",
+                      style: TextStyle(
+                        fontSize: 25.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 3.0,
+                    ),
+                    Text(
+                      "Software Engineer",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 7.0,
+                    ),
+                    Text(
+                      "Hello, I am Software Engineer at Namesace Inc. I spefically work of web development. But are is not limited. Sometimes I work on AWS. And sometimes I work on frontend using React too. I have worked on many projects using gulp.",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                      ),
+                    ),
                   ],
                 ),
               ),
